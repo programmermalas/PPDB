0c6e582d0f6a0c570c1de2e0ad2868cba6f33bf5
Irfan Hadian <irfan.hadian10@gmail.com>
2020-02-02 18:00:31 +0700

    added service provider barryvdh/laravel-dompdf

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Baryyvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Baryyvdh\DomPDF\Facade::class,
 
     ],
 
